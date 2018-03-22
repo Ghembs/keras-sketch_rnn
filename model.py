df159a7d5cf674c4365fb35ce9e506d67e4ebd4a
Ghembs <ggambacorta88@gmail.com>
2018-03-22 18:08:46 +0100

    included mdn in general model

--- model.py
+++ model.py
@@ -7,13 +7,41 @@ Created on 19/02/2018
 
 from keras import backend as K
 from keras.engine.topology import Layer
-from edward.models import Normal, Categorical, Mixture
 import tensorflow as tf
 from keras.layers import Input, Dense, Lambda, LSTM, Dropout, Bidirectional, Reshape, concatenate
 from keras.models import Model
 from recurrentshop import RecurrentModel, LSTMCell
 import numpy as np
 import math
+
+
+class MixtureDensity(Layer):
+    def __init__(self, kernelDim, numComponents, **kwargs):
+        # self.hiddenDim = 24
+        self.kernelDim = kernelDim
+        self.numComponents = numComponents
+        super(MixtureDensity, self).__init__(**kwargs)
+
+    def build(self, inputShape):
+        self.inputDim = inputShape[2]
+        self.outputDim = self.numComponents * (1 + self.kernelDim) + 3
+        # self.Wh = K.variable(np.random.normal(scale=0.5, size = (self.inputDim, self.hiddenDim)))
+        # self.bh = K.variable(np.random.normal(scale=0.5, size = self.hiddenDim))
+        self.Wo = K.variable(np.random.normal(scale=0.5, size = (self.inputDim, self.outputDim)))
+        self.bo = K.variable(np.random.normal(scale=0.5, size = self.outputDim))
+
+        self.trainable_weights = [self.Wo, self.bo]  # [self.Wh, self.bh, self.Wo, self.bo]
+
+    def call(self, x, mask=None):
+        print x.shape[:]
+        # hidden = K.tanh(K.dot(x, self.Wh) + self.bh)
+        print self.Wo.shape[:]
+        print self.bo.shape[:]
+        output = K.dot(x, self.Wo) + self.bo
+        return output
+
+    def compute_output_shape(self, inputShape):
+        return inputShape[0], self.outputDim
 
 
 class Vae:
@@ -29,6 +57,7 @@ class Vae:
     dec_1 = Dense(5)
     dec_2 = Dense(123)
     dec_3 = LSTMCell(512)
+    mdn = MixtureDensity(5, 20)
 
     def __init__(self):
         a = self.enc_1(self.input)
@@ -59,10 +88,12 @@ class Vae:
 
         out = rnn(z_out, initial_state = [_h_1, _h_2])
 
-        out = self.dec_2(out)
-        out = self.dec_1(out)
+        # out = self.dec_2(out)
+        out = self.mdn(out)
 
         self.vae = Model(self.input, out)
+
+        print self.vae.summary()
 
     def sampling(self, args):
         z_mean, z_log_sigma = args
@@ -113,63 +144,4 @@ class Vae:
 #         return get_lossfunc(out_pi, out_sigma, out_mu, y)
 #     return loss
 
-class MixtureDensity(Layer):
-    def __init__(self, kernelDim, numComponents, **kwargs):
-        self.hiddenDim = 24
-        self.kernelDim = kernelDim
-        self.numComponents = numComponents
-        super(MixtureDensity, self).__init__(**kwargs)
-
-    def build(self, inputShape):
-        self.inputDim = inputShape[1]
-        self.outputDim = self.numComponents * (2+self.kernelDim)
-        self.Wh = K.variable(np.random.normal(scale=0.5, size = (self.inputDim, self.hiddenDim)))
-        self.bh = K.variable(np.random.normal(scale=0.5, size = self.hiddenDim))
-        self.Wo = K.variable(np.random.normal(scale=0.5, size = (self.hiddenDim, self.outputDim)))
-        self.bo = K.variable(np.random.normal(scale=0.5, size = self.outputDim))
-
-        self.trainable_weights = [self.Wh, self.bh, self.Wo, self.bo]
-
-    def call(self, x, mask=None):
-        hidden = K.tanh(K.dot(x, self.Wh) + self.bh)
-        output = K.dot(hidden, self.Wo) + self.bo
-        return output
-
-    def compute_output_shape(self, inputShape):
-        return inputShape[0], self.outputDim
-
-
-# class MixtureDensityNetwork:
-#     """
-#     Mixture density network for outputs y on inputs x.
-#     p((x,y), (z,theta))
-#     = sum_{k=1}^K pi_k(x; theta) Normal(y; mu_k(x; theta), sigma_k(x; theta))
-#     where pi, mu, sigma are the output of a neural network taking x
-#     as input and with parameters theta. There are no latent variables
-#     z, which are hidden variables we aim to be Bayesian about.
-#     """
-#
-#     hidden = Decoder()
-#
-#     def __init__(self, k):
-#         self.K = k  # here K is the amount of Mixtures
-#
-#     def mapping(self, x):
-#         """pi, mu, sigma = NN(x; theta)"""
-#         hidden1 = Dense(15, activation='relu')(x)  # fully-connected layer with 15 hidden units
-#         hidden2 = Dense(15, activation='relu')(hidden1)
-#         self.mus = Dense(self.K)(hidden2)  # the means
-#         self.sigmas = Dense(self.K, activation=K.exp)(hidden2)  # the variance
-#         self.pi = Dense(self.K, activation=K.softmax)(hidden2)  # the mixture components
-#
-#     def log_prob(self, xs, zs = None):
-#         """log p((xs,ys), (z,theta)) = sum_{n=1}^N log p((xs[n,:],ys[n]), theta)"""
-#         # Note there are no parameters we're being Bayesian about. The
-#         # parameters are baked into how we specify the neural networks.
-#         X, y = xs
-#         self.mapping(X)
-#         result = tf.exp(Normal.logpdf(y, self.mus, self.sigmas))
-#         result = tf.mul(result, self.pi)
-#         result = tf.reduce_sum(result, 1)
-#         result = tf.log(result)
-#         return tf.reduce_sum(result)
+cacca = Vae()
