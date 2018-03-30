73340953fac3d37b0352bc82005c28cd84ad3154
ghembs <ggambacorta88@gmail.com>
2018-03-30 03:13:51 +0200

    improved training results

--- model.py
+++ model.py
@@ -9,7 +9,7 @@ from keras import backend as K
 from keras.engine.topology import Layer
 from keras.layers import Input, Dense, Lambda, LSTM, Dropout, Bidirectional, Reshape, concatenate
 from keras.models import Model
-from recurrentshop import RecurrentModel, LSTMCell
+from recurrentshop import LSTMCell, RecurrentModel
 import numpy as np
 
 
@@ -25,6 +25,13 @@ class MixtureDensity(Layer):
         self.Wo = K.variable(np.random.normal(scale=0.5, size = (self.inputDim, self.outputDim)))
         self.bo = K.variable(np.random.normal(scale=0.5, size = self.outputDim))
 
+        # self.Wo = self.add_weight(name = 'mixture_wo', shape = (self.inputDim, self.outputDim),
+        #                           initializer = 'glorot_uniform', trainable = True)
+        # self.bo = self.add_weight(name = 'mixture_bo', shape = (self.outputDim,),
+        #                           initializer = 'glorot_uniform', trainable = True)
+
+        super(MixtureDensity, self).build(inputShape)
+
         self.trainable_weights = [self.Wo, self.bo]
 
     def call(self, x, mask=None):
@@ -32,12 +39,12 @@ class MixtureDensity(Layer):
         return output
 
     def compute_output_shape(self, inputShape):
-        return inputShape[0], self.outputDim
+        return inputShape[0], inputShape[1], self.outputDim
 
 
+# TODO check activation functions
 class Vae:
-    max_len = 250
-    input = Input((max_len, 5,))
+    dec_input = Input(shape = (128,))
     decoder_input = Input(shape = (133,))
     h_in = Input(shape = (512,))
     c_in = Input(shape = (512,))
@@ -45,34 +52,46 @@ class Vae:
     enc_1 = Bidirectional(LSTM(256))
     enc_mean = Dense(128)
     enc_log_sigma = Dense(128)
-    h_init = Dense(1024)
+    h_init = Dense(1024, activation = 'tanh')
     dec_1 = LSTM(512)
     dec_3 = LSTMCell(512)
     mdn = MixtureDensity(5, 20)
 
-    def __init__(self, generate = False):
-        self.h_out = None
-        self.c_out = None
+    def __init__(self, generate = False, max_len = 250):
+        self.generate = generate
+        self.max_len = max_len
+        self.input = Input((max_len, 5,))
 
-        if not generate:
+        self.decoder = self.build_decoder()
+
+        if not self.generate:
             self.encoder = self.build_encoder()
             self.mean, self.log_sigma = self.encoder(self.input)
             self.z = Lambda(self.sampling)([self.mean, self.log_sigma])
-        else:
-            self.z = Input(shape = (128,))
+            out, self.h_out, self.c_out = self.decoder([self.z, self.input])
+            self.vae = Model(self.input, out)
 
-        if self.h_out is None:
-            _h = self.h_init(self.z)
+            print(self.vae.summary())
 
-            _h = Reshape((512, 2,))(_h)
+    def tile(self, tensor):
+        return K.tile(tensor, [1, self.max_len, 1])
 
-            _h_1 = Lambda(lambda x: x[:, :, 0])(_h)
-            _c_1 = Lambda(lambda x: x[:, :, 1])(_h)
-        else:
-            _h_1 = self.h_out
-            _c_1 = self.c_out
+    def build_encoder(self):
+        a = self.enc_1(self.input)
+        mean = self.enc_mean(a)
+        log_sigma = self.enc_log_sigma(a)
+        encoder = Model(self.input, [mean, log_sigma])
+        return encoder
 
-        z_ = Reshape((1, 128,))(self.z)
+    def build_decoder(self):
+        _h = self.h_init(self.dec_input)
+
+        _h = Reshape((512, 2,))(_h)
+
+        _h_1 = Lambda(lambda x: x[:, :, 0])(_h)
+        _c_1 = Lambda(lambda x: x[:, :, 1])(_h)
+
+        z_ = Reshape((1, 128,))(self.dec_input)
         z_ = Lambda(self.tile)(z_)
 
         z_ = concatenate([z_, self.input], axis = 2)
@@ -86,28 +105,13 @@ class Vae:
                              output = dec_out, final_states = [h, c],
                              return_sequences = True, return_states = True)
 
-        out, self.h_out, self.c_out = rnn(z_, initial_state = [_h_1, _c_1])
-
-        out = Reshape((-1, 512,))(out)
+        out, h_out, c_out = rnn(z_, initial_state = [_h_1, _c_1])
 
         out = self.mdn(out)
 
-        if generate:
-            self.vae = Model([self.z, self.input], out)
-        else:
-            self.vae = Model(self.input, out)
+        decoder = Model([self.dec_input, self.input], [out, h_out, c_out])
 
-        print(self.vae.summary())
-
-    def tile(self, tensor):
-        return K.tile(tensor, [1, self.max_len, 1])
-
-    def build_encoder(self):
-        a = self.enc_1(self.input)
-        mean = self.enc_mean(a)
-        log_sigma = self.enc_log_sigma(a)
-        encoder = Model(self.input, [mean, log_sigma])
-        return encoder
+        return decoder
 
     def sampling(self, args):
         z_mean, z_log_sigma = args
--- vae.py
+++ vae.py
@@ -8,38 +8,43 @@ Created on 20/02/2018
 import numpy as np
 # from keras import metrics
 import keras.backend as K
-# from keras.layers import subtract, multiply
+from keras import optimizers
 import dataloader as dl
 import model as vae
 import random
 
-
+# TODO keep checking if better with max_pi, max_q, check with simple K.softmax too
 def get_mixture_coef(output):
-    out_pi = output[:, 0, :20]
-    out_mu_x = output[:, 0, 20:40]
-    out_mu_y = output[:, 0, 40:60]
-    out_sigma_x = output[:, 0, 60:80]
-    out_sigma_y = output[:, 0, 80:100]
-    out_ro = output[:, 0, 100:120]
-    pen_logits = output[:, 0, 120:123]
+    out_pi = output[:, :20]
+    out_mu_x = output[:, 20:40]
+    out_mu_y = output[:, 40:60]
+    out_sigma_x = output[:, 60:80]
+    out_sigma_y = output[:, 80:100]
+    out_ro = output[:, 100:120]
+    pen_logits = output[:, 120:123]
     # use softmax to normalize pi and q into prob distribution
     # max_pi = K.max(out_pi, axis = 1, keepdims=True)
     # out_pi = out_pi - max_pi
-    # out_pi = K.exp(out_pi)
-    # normalize_pi = 1 / K.sum(out_pi, axis=1, keepdims=True)
-    # out_pi = normalize_pi * out_pi
-    out_pi = K.softmax(out_pi)
-    out_q = K.softmax(pen_logits)
+    out_pi = K.exp(out_pi)
+    normalize_pi = 1 / (K.sum(out_pi, axis=1, keepdims=True))
+    out_pi = normalize_pi * out_pi
+    # out_pi = K.softmax(out_pi)
+    # out_q = K.softmax(pen_logits)
+    # max_q = K.max(pen_logits, axis = 1, keepdims = True)
+    # out_q = pen_logits - max_q
+    out_q = K.exp(pen_logits)
+    normalize_q = 1 / (K.sum(out_q, axis = 1, keepdims = True))
+    out_q = normalize_q * out_q
     out_ro = K.tanh(out_ro)
     # use exponential to make sure sigma is positive
     out_sigma_x = K.exp(out_sigma_x)
     out_sigma_y = K.exp(out_sigma_y)
-    return out_pi, out_mu_x, out_mu_y, out_sigma_x, out_sigma_y, out_ro, pen_logits
+    return out_pi, out_mu_x, out_mu_y, out_sigma_x, out_sigma_y, out_ro, pen_logits, out_q
 
 
 def tf_bi_normal(x, y, mu_x, mu_y, sigma_x, sigma_y, ro):
-    x_ = K.reshape(x, (100, 1))
-    y_ = K.reshape(y, (100, 1))
+    x_ = K.reshape(x, (-1, 1))
+    y_ = K.reshape(y, (-1, 1))
     norm1 = x_ - mu_x
     norm2 = y_ - mu_y
     sigma = sigma_x * sigma_y
@@ -52,11 +57,11 @@ def tf_bi_normal(x, y, mu_x, mu_y, sigma_x, sigma_y, ro):
     return result
 
 
-# TODO sort dimensions out
 def get_lossfunc(out_pi, out_mu_x, out_mu_y, out_sigma_x, out_sigma_y, out_ro, out_q, x, y, logits):
     # L_r loss term calculation, L_s part
     result = tf_bi_normal(x, y, out_mu_x, out_mu_y, out_sigma_x, out_sigma_y, out_ro)
     result = result * out_pi
+    # result = K.permute_dimensions(result, [0, 2, 1])
     result = K.sum(result, axis=1, keepdims=True)
     result = -K.log(result + 1e-8)
     fs = 1.0 - logits[:, 2]
@@ -70,7 +75,7 @@ def get_lossfunc(out_pi, out_mu_x, out_mu_y, out_sigma_x, out_sigma_y, out_ro, o
 
 
 def mdn_loss(x, y, pen, output):
-    out_pi, out_mu_x, out_mu_y, out_sigma_x, out_sigma_y, out_ro, out_q = get_mixture_coef(
+    out_pi, out_mu_x, out_mu_y, out_sigma_x, out_sigma_y, out_ro, out_q, o_pen = get_mixture_coef(
         output)
     L_r = get_lossfunc(out_pi, out_mu_x, out_mu_y, out_sigma_x,
                         out_sigma_y, out_ro, out_q, x, y, pen)
@@ -83,12 +88,9 @@ class Compiler:
     original_dim = 250
     epochs = 5
 
-    def __init__(self, names, generate = False):
-        self.vae = vae.Vae(generate)
-        if not generate:
-            # TODO check if mean and log_sigma assigned this way are static
-            self.z_mean = self.vae.mean
-            self.z_log_sigma = self.vae.log_sigma
+    def __init__(self, names, generate = False, max_len = 250):
+        self.generate = generate
+        self.vae = vae.Vae(self.generate, max_len)
         self.x_train = None
         self.x_valid = None
         self.x_test = None
@@ -96,7 +98,6 @@ class Compiler:
 
     def load_dataset(self, names):
         for name in names:
-            # TODO check validity of "bytes" encoding
             dataset = np.load("Dataset/" + name + ".full.npz", encoding = 'bytes')
             if self.x_train is None:
                 self.x_train = dataset["train"][:10000]
@@ -107,7 +108,8 @@ class Compiler:
                 self.x_valid = np.concatenate((self.x_valid, dataset["valid"]))
                 self.x_test = np.concatenate((self.x_test, dataset["test"]))
 
-        self.x_train = dl.DataLoader(self.x_train, self.batch_size)
+        self.x_train = dl.DataLoader(self.x_train, self.batch_size,
+                                     random_scale_factor = 0.15, augment_stroke_prob = 0.1)
         normal_scale_factor = self.x_train.calculate_normalizing_scale_factor()
         self.x_train.normalize(normal_scale_factor)
 
@@ -122,10 +124,11 @@ class Compiler:
         val_x = target[:, 0]
         val_y = target[:, 1]
         pen = target[:, 2:]
-        rec_loss = mdn_loss(val_x, val_y, pen, x_decoded)
+        output = K.reshape(x_decoded, [-1, 123])
+        rec_loss = mdn_loss(val_x, val_y, pen, output)
         try:
-            kl_loss = - 0.5 * K.mean(1 + self.z_log_sigma - K.square(self.z_mean) -
-                                     K.exp(self.z_log_sigma), axis = -1)
+            kl_loss = - 0.5 * K.mean(1 + self.vae.log_sigma - K.square(self.vae.mean) -
+                                     K.exp(self.vae.log_sigma), axis = -1)
             return rec_loss + 1 * kl_loss
         except AttributeError:
             print("no encoder")
@@ -152,34 +155,38 @@ class Compiler:
 
     def load_weights(self):
         try:
-            self.vae.vae.load_weights("vae_model")
+            if not self.generate:
+                self.vae.encoder.load_weights("vae_enc")
+            self.vae.decoder.load_weights("vae_dec")
             print("Loaded weights from file")
         except IOError:
             print("Weights not found")
 
+    # TODO check if useful to separate the losses, training on single batches and calculating them
+    # TODO on the go
     def compile_fit(self):
-        self.vae.vae.compile(loss = self.vae_loss, optimizer = 'adam')
+        adam = optimizers.Adam(lr = 0.001, beta_1 = 0.9, beta_2 = 0.999,
+                               decay = 0.999)
+        self.vae.vae.compile(loss = self.vae_loss, optimizer = adam, metrics = ['accuracy'])
 
         self.load_weights()
 
         batches, val_batches = self.set_batches()
 
-        print(batches.shape[:])
-        print(val_batches.shape[:])
-
-        _batches = np.reshape(batches, [-1, 5])
-        _batches = _batches[:20000, :]
-        _val_batches = np.reshape(val_batches, [-1, 5])
-        _val_batches = _val_batches[:2000, :]
-
-        self.vae.vae.fit(batches, _batches, shuffle = False,
+        # _batches = np.reshape(batches, [-1, 5])
+        # _batches = _batches[:5000, :]
+        # _val_batches = np.reshape(val_batches, [-1, 5])
+        # _val_batches = _val_batches[:1000, :]
+        # TODO check best shuffle value
+        self.vae.vae.fit(batches, batches, shuffle = False,
                          batch_size = self.batch_size, epochs = self.epochs,
-                         validation_data = (val_batches, _val_batches))
-        self.vae.vae.save_weights("vae_model", True)
+                         validation_data = (val_batches, val_batches))
+        self.vae.encoder.save_weights("vae_enc", True)
+        self.vae.decoder.save_weights("vae_dec", True)
 
 
 def adjust_temp(pi_pdf, temp):
-    pi_pdf = np.log(pi_pdf) / temp
+    pi_pdf = np.log(pi_pdf + 1e-8) / (temp + 1e-8)
     pi_pdf -= pi_pdf.max()
     pi_pdf = np.exp(pi_pdf)
     pi_pdf /= pi_pdf.sum()
@@ -204,13 +211,16 @@ def sample_gaussian_2d(mu1, mu2, s1, s2, rho, temp=1.0, greedy=False):
     if greedy:
         return mu1, mu2
     mean = [mu1, mu2]
+    print(mean)
     s1 *= temp * temp
     s2 *= temp * temp
     cov = [[s1 * s1, rho * s1 * s2], [rho * s1 * s2, s2 * s2]]
+    print(cov)
     x = np.random.multivariate_normal(mean, cov, 1)
     return x[0][0], x[0][1]
 
 
+# TODO check why end of sketch comes too soon/randomly
 def sample(model, seq_len=250, temperature=1.0, greedy_mode=False,
            z=None):
     """Samples a sequence from a pre-trained model."""
@@ -226,11 +236,13 @@ def sample(model, seq_len=250, temperature=1.0, greedy_mode=False,
     temp = 1.0
 
     for i in range(seq_len):
+        print(i)
 
         sample, h_out, c_out = model.predict([z, prev_x])
 
-        params = get_mixture_coef(sample)
-        [o_pi, o_mu1, o_mu2, o_sigma1, o_sigma2, o_corr, o_pen] = params
+        sample = K.reshape(sample, [1, -1])
+
+        o_pi, o_mu1, o_mu2, o_sigma1, o_sigma2, o_corr, out_q, o_pen = get_mixture_coef(sample)
 
         if i < 0:
             greedy = False
@@ -239,15 +251,15 @@ def sample(model, seq_len=250, temperature=1.0, greedy_mode=False,
             greedy = greedy_mode
             temp = temperature
 
-        idx = get_pi_idx(random.random(), o_pi[0], temp, greedy)
+        idx = get_pi_idx(random.random(), K.eval(o_pi[0]), temp, greedy)
 
-        idx_eos = get_pi_idx(random.random(), o_pen[0], temp, greedy)
+        idx_eos = get_pi_idx(random.random(), K.eval(o_pen[0]), temp, greedy)
         eos = [0, 0, 0]
         eos[idx_eos] = 1
 
-        next_x1, next_x2 = sample_gaussian_2d(o_mu1[0][idx], o_mu2[0][idx],
-                                              o_sigma1[0][idx], o_sigma2[0][idx],
-                                              o_corr[0][idx], np.sqrt(temp), greedy)
+        next_x1, next_x2 = sample_gaussian_2d(K.eval(o_mu1[0][idx]), K.eval(o_mu2[0][idx]),
+                                              K.eval(o_sigma1[0][idx]), K.eval(o_sigma2[0][idx]),
+                                              K.eval(o_corr[0][idx]), np.sqrt(temp), greedy)
 
         strokes[i, :] = [next_x1, next_x2, eos[0], eos[1], eos[2]]
 
@@ -262,17 +274,14 @@ def sample(model, seq_len=250, temperature=1.0, greedy_mode=False,
         prev_x[0][0] = np.array(
             [next_x1, next_x2, eos[0], eos[1], eos[2]], dtype=np.float32)
 
-        model.h_out = h_out
-        model.c_out = c_out
-
     return strokes, mixture_params
 
 
 def draw():
-    vae = Compiler(["cat", "flying_saucer"], True)
+    vae = Compiler(["cat", "flying_saucer"], True, 1)
     vae.load_weights()
 
-    stroke_, m = sample(vae.vae.vae, 250, 1, False, None)
+    stroke_, m = sample(vae.vae.decoder, 100, 1, False, None)
 
     # sample = vae.x_train.random_sample()
     # sample = dl.to_big_strokes(sample)
@@ -280,7 +289,7 @@ def draw():
     # stroke_ = vae.vae.vae.predict(sample)
     print(stroke_)
     print(stroke_.shape[:])
-    stroke_ = stroke_.reshape(stroke_.shape[1], stroke_.shape[2])
+    # stroke_ = stroke_.reshape(stroke_.shape[0], stroke_.shape[1])
 
     stroke_ = dl.to_normal_strokes(stroke_)
     dl.draw_strokes(stroke_)
