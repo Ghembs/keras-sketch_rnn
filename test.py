b61975a05c02ad22b0b77d181b749b003b115dba
ghembs <ggambacorta88@gmail.com>
2018-04-05 01:20:06 +0200

    modified test class

--- test.py
+++ test.py
@@ -12,8 +12,9 @@ https://github.com/tensorflow/magenta-demos/blob/master/jupyter-notebooks/Sketch
 
 # import the required libraries
 from six.moves import xrange
+import sys
 
-# libraries required for visualisation:
+# libraries required for visualisation
 from IPython.display import SVG, display
 import svgwrite
 # import our command line tools
@@ -24,12 +25,14 @@ from magenta.models.sketch_rnn.rnn import *
 
 # set numpy output to something sensible
 np.set_printoptions(precision=8, edgeitems=6, linewidth=200, suppress=True)
+
+# Path for the sketch-rnn folder
 path = 'D:/Documenti/UNI/magenta/magenta/models/sketch_rnn/'
 
 
 # =========================== UTILITIES ======================================
 # little function that displays vector images and saves them to .svg
-def draw_strokes(data, factor=0.2, svg_filename = path + 'sample.svg'):
+def draw_strokes(data, factor=0.2, svg_filename = 'sample.svg'):
     tf.gfile.MakeDirs(os.path.dirname(svg_filename))
     min_x, max_x, min_y, max_y = get_bounds(data, factor)
     dims = (50 + max_x - min_x, 50 + max_y - min_y)
@@ -106,9 +109,10 @@ def interpolation(z0, z1, spherical = True, n = 10):
 # ==================================== CLASS ======================================
 class Tester:
 
-    def __init__(self, data, model, conditional = True):
+    def __init__(self, data, model, max_seq_len, conditional = True):
 
         self.cond = conditional
+        self.max_seq_len = max_seq_len
         data_dir = path + data
         model_dir = path + model
         if conditional:
@@ -129,17 +133,18 @@ class Tester:
         # loads the weights from checkpoint into our model
         load_checkpoint(self.sess, model_dir)
 
-    def encode(self, input_strokes, seq_len):
+    def encode(self, input_strokes, name = 'enc_sample.svg'):
         strokes = to_big_strokes(input_strokes).tolist()
         strokes.insert(0, [0, 0, 1, 0, 0])
-        strokes = strokes[:seq_len]
+        strokes = strokes[:self.max_seq_len]
         seq_len = [len(input_strokes)]
-        draw_strokes(to_normal_strokes(np.array(strokes)))
+        draw_strokes(to_normal_strokes(np.array(strokes)), svg_filename = name)
         return self.sess.run(self.eval_model.batch_z,
                              feed_dict={self.eval_model.input_data: [strokes],
                                         self.eval_model.sequence_lengths: seq_len})[0]
 
-    def decode(self, z_input=None, draw_mode=True, temperature=0.1, factor=0.2):
+    def decode(self, z_input=None, draw_mode=True, temperature=0.2, factor=0.2,
+               name = 'dec_sample.svg'):
         z = None
         if z_input is not None:
             z = [z_input]
@@ -148,54 +153,169 @@ class Tester:
                                    temperature=temperature, z=z)
         strokes = to_normal_strokes(sample_strokes)
         if draw_mode:
-            draw_strokes(strokes, factor)
+            draw_strokes(strokes, factor, svg_filename = name)
         return strokes
 
-    def draw_stroke_list(self, latent = None, vary_temp = False, start_temp = 0.5, n = 10):
+    def draw_stroke_list(self, latent = None, start_temp = 0.5, n = 10):
         reconstructions = []
         for i in range(n):
             if self.cond:
                 if type(latent) is list:
+                    name = 'interpolated_sample.svg'
                     # for every latent vector in latent, sample a vector image
-                    reconstructions.append([self.decode(latent[i], draw_mode = False), [0, i]])
+                    reconstructions.append([self.decode(latent[i], draw_mode = False),
+                                            [0, i]])
                 else:
+                    name = 'temperature_sample.svg'
                     reconstructions.append([self.decode(latent, draw_mode = False,
-                                            temperature = 0.1 * i + vary_temp), [0, i]])
+                                            temperature = 0.1 * i + 0.1),
+                                            [0, i]])
             else:
+                name = 'unconditioned_sample.svg'
                 # randomly unconditionally generate n examples
                 reconstructions.append([self.decode(temperature = start_temp, draw_mode = False),
                                         [0, i]])
 
         stroke_grid = make_grid_svg(reconstructions)
-        draw_strokes(stroke_grid)
+        draw_strokes(stroke_grid, svg_filename = name)
 
 
-def initialize_vae():
-    manual = "Please choose the model you'd like to test\n"
+def load_dataset(dataset):
+    dataset = path + "datasets/" + dataset + ".full.npz"
 
-    i = -1
-    while i < 0:
+    test = np.load(dataset, encoding = 'bytes')["test"]
+
+    test = DataLoader(test)
+    scale_factor = test.calculate_normalizing_scale_factor()
+    test.normalize(scale_factor)
+
+    return test
+
+
+def extract_stroke(model, dataset, wrong = False):
+    if wrong:
+        test = load_dataset(dataset)
+        stroke = test.random_sample()
+    else:
+        stroke = model.test_set.random_sample()
+
+    return stroke
+
+
+def conditional_generation(model, operation, temp, datasets, wrong = False):
+    stroke = extract_stroke(model, datasets[0], wrong)
+    z0 = model.encode(stroke, name = 'z0_sample.svg')
+
+    if operation == 0:
+        _ = model.decode(z0, temperature = temp)
+    else:
+        if operation == 2:
+            stroke = extract_stroke(model, datasets[1], wrong)
+            z1 = model.encode(stroke, name = 'z1_sample.svg')
+
+            z0 = interpolation(z0, z1)
+
+        model.draw_stroke_list(z0)
+
+
+def unconditional_generation(model, temp):
+    model.draw_stroke_list(start_temp = temp)
+
+
+def run_test(framework, arguments):
+    try:
+        temp = float(arguments[0])
+    except (ValueError, IndexError):
+        temp = 0.5
+
+    if temp < 0:
+        temp = 0
+    elif temp > 1:
+        temp = 1
+
+    if framework.cond:
         try:
-            i = int(input(manual))
-        except ValueError:
-            print("Please insert a number\n")
+            operation = int(arguments[1])
+        except (ValueError, IndexError):
+            operation = 0
+        try:
+            wrong = bool(arguments[2])
+        except (ValueError, IndexError):
+            wrong = False
+
+        dataset1 = ''
+        dataset2 = ''
+
+        if wrong:
+            try:
+                dataset1 = arguments[3]
+            except (ValueError, IndexError):
+                dataset1 = 'cookie'
+            if operation == 2:
+                try:
+                    dataset2 = arguments[4]
+                except (ValueError, IndexError):
+                    dataset2 = 'sword'
+        conditional_generation(framework, operation, temp, [dataset1, dataset2], wrong)
+
+    else:
+        unconditional_generation(framework, temp)
+
+
+def main(arguments):
+    try:
+        i = int(arguments[0])
+    except ValueError:
+        i = 0
 
     if i == 0:
-        framework = Tester('datasets', 'cat_saucer_model')
+        framework = Tester('datasets', 'cat_saucer_model', 130)
     elif i == 1:
-        framework = Tester('datasets', 'owl_model', False)
+        framework = Tester('datasets', 'little_prince_model', 147)
     else:
-        framework = Tester('datasets', 'little_prince_model')
+        i = 2
+        framework = Tester('datasets', 'owl_model', False)
 
-    return framework
+    try:
+        run_test(framework, arguments[1:])
+    except IndexError:
+        run_test(framework, [0.5])
+
+    message = "Run more tests?\n " \
+              "(insert new parameters except for [model], leave empty to end program)\n"
+    more = True
+
+    while more:
+        params = input(message).split()
+        if len(params) == 0:
+            more = False
+        else:
+            run_test(framework, params)
 
 
 if __name__ == "__main__":
-    welcome = "This is a tool to experiment with some sketch rnn implementation.\n"\
-              "The available models are:\n"\
-              "0 - A simple VAE trained on cats and flying saucers\n"\
-              "1 - An auto-regressive unconditional generator\n"\
-              "2 - A complex VAE trained on elephants, hats and snakes\n"
+    if len(sys.argv) == 1:
+        welcome = "This is a tool to experiment with some sketch-rnn implementations.\n"\
+                   "Arguments: "\
+                   "[model] [temperature] [operation] [custom] [dataset1] [dataset2]\n\n"\
+                   "[model] an integer to choose which model to test:\n"\
+                   "\t 0 - Simple VAE trained on cats and flying saucers\n"\
+                   "\t 1 - Complex VAE trained on elephants, hats and snakes\n"\
+                   "\t 2 - Autoregressive RNN trained on owls\n\n" \
+                   "[temperature] a float in range (0.0, 1.0), temperature parameter for " \
+                   "the model.\n\n"\
+                   "[operation] an integer to choose what to generate (only required " \
+                   "for VAEs):\n"\
+                   "\t 0 - Conditional generation\n"\
+                   "\t 1 - Conditional reconstruction (varying temperature)\n"\
+                   "\t 2 - Conditional interpolation\n\n"\
+                   "[custom] a boolean to choose if to generate from default sketches " \
+                   "(only for VAEs).\n\n"\
+                   "[dataset1] name of a dataset in path/datasets folder " \
+                   "(only if [custom] == True).\n\n"\
+                   "[dataset2] name of a dataset in path/datasets folder " \
+                   "(only if [custom] == True and [operation] == 2).\n"
 
-    print(welcome)
-    model = initialize_vae()
+        print(welcome)
+    else:
+        main(sys.argv[1:])
